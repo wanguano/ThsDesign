ebf1a1d4b3464bfb03db10f4dc648aa776dd7c28
吴建明 <wujm@interal.ths.com.cn>
2024-10-21 22:52:45 +0800

    feat: 更新接口地址

--- src/service/config.js
+++ src/service/config.js
@@ -1,9 +1,18 @@
+/*
+ * @Description: 
+ * @Author: 吴建明
+ * @Date: 2024-10-21 22:34:09
+ * @LastEditors: 吴建明
+ * @LastEditTime: 2024-10-21 22:51:41
+ * @FilePath: \ThsDesign\src\service\config.js
+ * 
+ */
 // 本地测试API
 // const devBaseURL = 'http://localhost:3000'
 // const proBaseURL = 'http://localhost:3000'
 // 已经部署到服务器上的API
-const devBaseURL = 'http://23.224.55.27:3000'
-const proBaseURL = 'http://23.224.55.27:3000'
+const devBaseURL = 'http://45.139.193.170:3000'
+const proBaseURL = 'http://45.139.193.170:3000'
 export const BASE_URL =
   process.env.NODE_ENV === 'development' ? devBaseURL : proBaseURL
 
